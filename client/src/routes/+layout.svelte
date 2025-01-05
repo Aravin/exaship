5b01e3eef4091baacb3843d22f5517d3896d27b8
Aravind A <4869265+Aravin@users.noreply.github.com>
2025-01-05 22:45:02 +0530

    Added basic nav

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,23 +1,52 @@
 <script lang="ts">
 	import '../app.css';
+	import 'flowbite';
 	let { children } = $props();
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+	import { Footer, FooterCopyright, FooterLinkGroup, FooterLink } from 'flowbite-svelte';
+	import { Breadcrumb, BreadcrumbItem } from 'flowbite-svelte';
+
+	import { page } from '$app/stores';
+	import { derived } from 'svelte/store';
+
+	const activeUrl = derived(page, $page => $page.url.pathname);
 </script>
 
 <header>
-    <h1>My Application Header</h1>
-    <nav>
-        <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/contact">Contact</a></li>
-        </ul>
-    </nav>
+	<Navbar>
+		<NavBrand href="/">
+			<img src="/images/flowbite-svelte-icon-logo.svg" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" />
+			<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite</span>
+		</NavBrand>
+		<NavHamburger />
+		<NavUl {activeUrl}>
+			<NavLi href="/">Home</NavLi>
+			<NavLi href="/navbar">Navbar</NavLi>
+			<NavLi href="/accordion">Accordion</NavLi>
+			<NavLi href="/alert">Alert</NavLi>
+			<NavLi href="/avatar">Avatar</NavLi>
+		</NavUl>
+	</Navbar>
 </header>
 
+<Breadcrumb aria-label="Default breadcrumb example">
+	<BreadcrumbItem href="/" home>Home</BreadcrumbItem>
+	<BreadcrumbItem href="/">Projects</BreadcrumbItem>
+	<BreadcrumbItem>Flowbite Svelte</BreadcrumbItem>
+  </Breadcrumb>
+
 <main>
-    {@render children()}
+	{@render children()}
 </main>
 
-<footer>
-    <p>© 2023 My Application. All rights reserved.</p>
-</footer>
+<Footer>
+	<div class="sm:flex sm:items-center sm:justify-between">
+		<FooterCopyright href="/" by="Flowbite™" year={2022} />
+		<FooterLinkGroup ulClass="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+			<FooterLink href="/">About</FooterLink>
+			<FooterLink href="/">Privacy Policy</FooterLink>
+			<FooterLink href="/">Licensing</FooterLink>
+			<FooterLink href="/">Contact</FooterLink>
+		</FooterLinkGroup>
+	</div>
+</Footer>
